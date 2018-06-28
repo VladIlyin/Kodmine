7e8fbb04943b7f8f30740d8ad024b42d35a920b9
Vladislav Ilyin <vlad.anich@gmail.com>
2018-06-28 11:48:56 +0300

    Извлечение секции HtmlCleanerRegEx из appsettings

--- Kodmine/Helpers/SettingsHelper.cs
+++ Kodmine/Helpers/SettingsHelper.cs
@@ -10,21 +10,14 @@ namespace Kodmine.Helpers
     {
         public static IDictionary<string, string> GetHtmlCleanerRegex(IConfiguration config)
         {
+            List<List<string>> HtmlCleanerRegExList = new List<List<string>>();
+            config.GetSection("HtmlCleanerRegEx").Bind(HtmlCleanerRegExList);
+
             Dictionary<string, string> dict = new Dictionary<string, string>();
-            string val = "", key = "";
-            byte i = 0;
 
-            while (key != null)
+            foreach (var item in HtmlCleanerRegExList)
             {
-                key = config[$"HtmlCleanerRegEx:{i}:0"];
-                val = config[$"HtmlCleanerRegEx:{i}:1"];
-
-                if (key != null)
-                {
-                    dict.Add(key, val);
-                }
-
-                i++;
+                dict.Add(item[0], item[1]);
             }
 
             return dict;
